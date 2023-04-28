5e3daf370d130c0d86cf7eadd26d0bbd0e8b43c2
Johnnie RaAl <jjuan.raal@hotmail.com>
2023-04-28 16:20:00 -0600

    modiefied to allow multiple select with one click

--- custom_select/c_select.js
+++ custom_select/c_select.js
@@ -4,9 +4,24 @@ class CustomSelect extends HTMLSelectElement {
         self = super();
         //obtenemos las opciones que añadieron en el select
         const opts = Array.from(this.querySelectorAll('option'));
-        
+          this.addEventListener('click', (event) => {
+                event.preventDefault();
+                event.stopPropagation();
+                
+                if(event.target.tagName == 'OPTION') {
+                    console.log(event.target.value);
+                    if(this.options.includes(event.target.value)){
+                        const newops = this.options.filter((x) => x != event.target.value);
+                        this.options = newops.map ((x) => x);
+                    } else {
+                        this.options.push(event.target.value);
+                    }
+                    this.updateoptions()
+                }
+            });
         //console.log(opts);
         opts.forEach(option => {
+            option.selected = false;
             const label = option.label;
             option.innerHTML = '';
             //console.log(label);
@@ -20,21 +35,34 @@ class CustomSelect extends HTMLSelectElement {
             divcont.appendChild(check);
             divcont.appendChild(labelv);
             option.appendChild(divcont);
-            
-            option.addEventListener('click', (event) => {
+             this.addEventListener('checked', (event) => {
                 event.preventDefault();
                 event.stopPropagation();
-                this.options.push(option);
                 
+                if(event.target.tagName == 'OPTION') {
+                    //console.log(event.target.value);
+                    if(this.options.includes(event.target.value)){
+                        const newops = this.options.filter((x) => x != event.target.value);
+                        this.options = newops.map ((x) => x);
+                        option.selected = false;
+                    } else {
+                        this.options.push(event.target.value);
+                         this.updateoptions();
+                    }
+                   
+                    
+                }
             });
-            
           });
        
     }
 
-    checkoption(target){
-        const opts = Array.from(this.querySelectorAll('option')).filter((x) => x.selected);
-        console.log(opts);
+    updateoptions(){
+        const opts = Array.from(this.querySelectorAll('option'));
+        
+        opts.forEach(option => {
+            option.selected = this.options.includes(option.value) ?  true : false;
+        })
     }
 }
 
